0c43a6ea54fe77ef27eebe74219b82968474e090
marioscience@hotmail.com <aLA2cf&3feb>
2014-03-30 20:30:37 -0600

    Template system is improved now. It could be implemented in one single file, but it is divided in two for added flexibility and functionality.

--- mdfiutah/urls.py
+++ mdfiutah/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mdfiutah.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^mdfserver/', include('mdfserver.urls')),
+    url(r'^', include('mdfserver.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
--- mdfserver/views.py
+++ mdfserver/views.py
@@ -7,7 +7,7 @@ from mdfserver.models import Page, Subpage
 def index(request):
     pages_in_server = Page.objects.all().order_by('-title')[:5]
     context = {'pages': pages_in_server}
-    return render(request, 'mdfserver/index.html', context)
+    return render(request, 'mdfserver/main_template.html', context)
 
 def pages(request, pages_passed):
     pages_in_server = Page.objects.all().order_by('-title')[:5]
